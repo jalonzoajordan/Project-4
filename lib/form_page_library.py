d0ab86d93a1f6a30ef576c3890cfe21cf0d623d2
unknown <jalonzoajordan@gmail.com>
2022-06-07 20:32:44 -0500

    added connection string to function

--- lib/form_page_library.py
+++ lib/form_page_library.py
@@ -2,10 +2,10 @@ import sqlalchemy
 from sqlalchemy import create_engine, inspect
 import os
 
-#set up engine
-engine = create_engine(f"sqlite:///{os.getcwd()}/Data/Poker_data.db")
-
 def card_list():
+    #set up engine
+    engine = create_engine(f"sqlite:///{os.getcwd()}/Data/Poker_data.db")
+    
     #run queries for data
     cards_query = engine.execute('SELECT DISTINCT table_1 from dbo_hands ORDER BY table_1').fetchall()
     cards_list = []
